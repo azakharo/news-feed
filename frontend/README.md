81dbe59878617bb6a5cf715c1f460d4506aa4422
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-03-02 13:36:03 +0300

    added frontend (just copied the starter)

---
+++ frontend/README.md
@@ -0,0 +1,10 @@
+# React + TypeScript + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+It uses [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) which uses [Babel](https://babeljs.io/)
+
+## React Compiler
+
+The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+import {Button} from 'flowbite-react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="mx-auto flex max-w-md items-center justify-center">
+      <div>
+        <h1 className="mb-4 text-center text-4xl">Vite + React</h1>
+        <div className="flex justify-center">
+          <Button color="green" onClick={() => setCount(count => count + 1)}>
+            count is {count}
+          </Button>
+        </div>
+        <div className="mt-2 text-2xl text-green-600">
+          This template uses Tailwind CSS
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
